a895a06287a60ae05b19474d10f2da33b19988ea
Peter Kreppein <pmkreppein@gmail.com>
2018-09-25 21:43:36 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,46 @@
-# Write your code here.
+def words_to_replace
+words_to_replace = {
+"too" => "2",
+"to" => "2",
+"two" =>"2",
+"four" => "4",
+"for" => "4",
+"be" => "b",
+"you" => "u",
+"at" => "@",
+"and" => "&"
+}
+end
+
+
+def word_substituter(tweet)
+tweet.split.map do |word|
+  if words_to_replace.keys.include?(word.downcase)
+    word = words_to_replace[word]
+  else
+    word
+  end
+end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+tweets.each do |tweet|
+  puts word_substituter(tweet)
+end
+end
+
+def selective_tweet_shortener(tweet)
+if tweet.length > 140
+  word_substituter(tweet)
+else
+  tweet
+end
+end
+
+def shortened_tweet_truncator(tweet)
+if word_substituter(tweet).length > 140
+  word_substituter(tweet)[0..136] + '...'
+else
+  tweet
+end
+end
